e5088e430fd03880ec50a5fb5add3d7337abfa8f
XDarner <144518832+XDarner@users.noreply.github.com>
2023-10-30 16:54:33 +0800

    Better option flow

--- src/views/start.vue
+++ src/views/start.vue
@@ -1,20 +1,28 @@
 <template>
-  <div>
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-container">
+    <!-- Background Carousel -->
+    <div id="carouselBackground" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
-        <div v-for="(question, index) in questions" :key="index" :class="{ 'carousel-item': true, active: index === 0 }">
-          <img :src="question.imageSrc" class="d-block w-100 image" :alt="question.imageAlt">
-          <div class="question">
-            {{ question.text }}
-          </div>
-          <div class="button">
-            <button v-for="(option, optionIndex) in question.options" :key="optionIndex" :class="option.type" @click="handleOptionClick(option)">
-              {{ option.text }}
-            </button>
+        <div v-for="(imageSrc, index) in carouselImages" :key="index" :class="{ 'carousel-item': true, active: index === 0 }">
+          <img :src="imageSrc" class="d-block w-100 image">
+          <!-- Content (Question and Options) in the middle of each carousel item -->
+          <div class="content">
+            <div class="question">
+              {{ scenarios[flow].text }}
+            </div>
+            <div class="options">
+              <button
+                v-for="(option, optionIndex) in scenarios[flow].options"
+                :key="optionIndex"
+                :class="option.type"
+                @click="handleOptionClick(option)"
+              >
+                {{ option.text }}
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <!-- Add your carousel controls here -->
     </div>
   </div>
 </template>
@@ -25,28 +33,28 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      flow: 1,
-      questions: [
+      flow: 0, // Initialize to show the first scenario
+      scenarios: [
         {
           text: "Question 1: Do you want us to recommend a secondary school for you?",
-          imageSrc: 'src/assets/CHS.jpg',
-          imageAlt: 'Image 1',
           options: [
-            { text: 'Yes', type: 'btn btn-primary' },
-            { text: 'No', type: 'btn btn-primary' },
+            { text: 'Yes', type: 'btn btn-custom' },
+            { text: 'No', type: 'btn btn-custom' },
           ],
         },
         {
           text: "Question 2: Another question text",
-          imageSrc: 'src/assets/NYGH.jpg',
-          imageAlt: 'Image 2',
           options: [
-            { text: 'Option A', type: 'btn btn-primary' },
-            { text: 'Option B', type: 'btn btn-primary' },
+            { text: 'Option A', type: 'btn btn-custom' },
+            { text: 'Option B', type: 'btn btn-custom' },
             // Add more options as needed
           ],
         },
         // Add more questions as needed
+      ],
+      carouselImages: [
+        'src/assets/CHS.jpg',
+        'src/assets/NYGH.jpg',
       ],
     };
   },
@@ -54,36 +62,48 @@ export default {
     handleOptionClick(option) {
       // Handle the click event for the selected option
       console.log('Selected option:', option);
-      // You can add logic here to handle different options as needed.
+
+      // Change the flow to the next scenario
+      this.flow++;
+
+      // You can add more logic here if needed, e.g., check for the end of scenarios.
     },
   },
 };
 </script>
 
 <style scoped>
-/* CSS for the overlay text and image fading */
-.question {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -80%);
-  text-align: center;
-  font-size: 30px;
-  color: #253028;
-  font-weight: bold;
+.carousel-container {
+  position: relative;
 }
 
-.button {
-  position: absolute;
-  top: 50%;
-  left: 50%;
+.carousel-item {
+  position: relative;
+  text-align: center; /* Center-align the content within each carousel item */
 }
 
 .image {
-  height: 500px;
-  opacity: 0.1;
+  width: 100%;
+  height: auto;
 }
-</style>
 
-  
-  
+.content {
+  position: absolute;
+  top: 50%; /* Center vertically */
+  left: 50%; /* Center horizontally */
+  transform: translate(-50%, -50%); /* Center both horizontally and vertically */
+  background: rgba(255, 255, 255, 0.8); /* Add a semi-transparent background to make text more readable */
+  padding: 20px; /* Add padding for spacing */
+}
+
+.question {
+  font-size: 24px;
+  color: #253028;
+}
+
+.options {
+  margin-top: 20px; /* Add spacing between question and options */
+}
+
+/* Other CSS styles for buttons */
+</style>
