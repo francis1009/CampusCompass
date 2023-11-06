648e1dc8e5644b947df429282c9123fdd1010902
XDarner <144518832+XDarner@users.noreply.github.com>
2023-11-07 04:26:29 +0800

    More info for school card

--- src/components/SchoolCard.vue
+++ src/components/SchoolCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+  <div>
     <!-- card -->
     <div class="card" style="width: 15rem; height: 23em; margin-left: 20px 20px;">
       <img :src="schoolImageUrl" class="card-img-top" alt="School Image" style="height:13rem">
@@ -11,16 +11,43 @@
     </div>
 
     <!-- dialog -->
-    <dialog ref="moreInfo" class="shadow-lg p-3 mb-5 bg-body rounded dialog-wrapper">
+    <dialog ref="moreInfo" class="shadow-lg p-3 mb-5 bg-body rounded">
       <div class="dialog">
-        <form class="flex-fill d-flex flex-column" method="dialog">
-          <p class="flex-fill">{{ schoolName }}</p>
-          <iframe :src=map_source height="260" width="260" scrolling="no" frameborder="0" allowfullscreen="allowfullscreen" style="width: 260px; height: 260px;"></iframe>
-          <menu class="mt-auto d-flex justify-content-center">
-            <button @click="closeDialog" class="btn btn-secondary">Close</button>
-            <button @click="sendtoindivpage" class="btn btn-secondary">Link to School Page</button>
-          </menu>
-        </form>
+        <div class="dialog-content">
+          <div class="school-logo">
+            <img :src="schoolImageUrl" alt="School Image" style="height: 13rem">
+          </div>
+          <div class="school-details">
+            <p class="school-name">{{ schoolName }}</p>
+            <div class="school-info">
+              <div>
+                <label>Region:</label>
+                {{ schoolRegion }}
+              </div>
+              <div>
+                <label>Address:</label>
+                {{ schoolAddress }}
+              </div>
+              <div>
+                <label>Nature:</label>
+                {{ schoolNature }}
+              </div>
+              <div>
+                <label>Type:</label>
+                {{ schoolType }}
+              </div>
+              <div>
+                <label>Website:</label>
+                {{ schoolwebsite }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <iframe :src="map_source" height="260" width="260" scrolling="no" frameborder="0" allowfullscreen="allowfullscreen" style="width: 260px; height: 260px;"></iframe>
+        <menu class="mt-auto d-flex justify-content-center">
+          <button @click="closeDialog" class="btn btn-secondary">Close</button>
+          <button @click="sendtoindivpage" class="btn btn-secondary">Link to School Page</button>
+        </menu>
       </div>
     </dialog>
   </div>
@@ -40,6 +67,11 @@ export default {
         return {
             schoolName: '',
             schoolAddress: '',
+            schoolRegion: '',
+            schoolMode: '',
+            schoolNature: '',
+            schoolType: '',
+            schoolwebsite: '',
             schoolImageUrl: '',
             one_map: '',
             school_postal_code: '',
@@ -55,6 +87,11 @@ export default {
                 .then(response => {
                     this.schoolName = response.data.School_Name;
                     this.schoolAddress = response.data.School_Address;
+                    this.schoolRegion = response.data.School_Region;
+                    this.schoolMode = response.data.School_Mode;
+                    this.schoolNature = response.data.School_Nature;
+                    this.schoolType = response.data.School_Type;
+                    this.schoolwebsite = response.data.School_Website;
                     var school_postal_code = response.data.School_Postal_Code.slice(1,7);
                     this.schoolImageUrl = response.data.School_Image_Source || this.schoolImageUrl;
                     this.map_source = `https://www.onemap.gov.sg/amm/amm.html?mapStyle=Night&zoomLevel=14&marker=postalcode:${school_postal_code}!colour:red&popupWidth=200`
@@ -79,7 +116,7 @@ export default {
 
 <style scoped>
 dialog {
-    position: fixed;
+    position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
@@ -87,15 +124,6 @@ dialog {
     height: 90%;
     border: none;
     border-radius: 5px;
-}
-
-.dialog {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
 }
 
 .btn {
