b6e7c1eb23378e88da9f621c8f652bd2c09979af
francis1009 <francism56789@gmail.com>
2023-11-06 20:31:04 +0800

    Change collapse

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -28,7 +28,7 @@
 import SidebarLink from './SidebarLink.vue'
 import { ref, computed } from 'vue'
 
-export const collapsed = ref(false)
+export const collapsed = ref(true)
 export const toggleSidebar = () => (collapsed.value = !collapsed.value)
 
 export const SIDEBAR_WIDTH = 180
