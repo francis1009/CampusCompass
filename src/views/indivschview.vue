06107a1aa7ea70f68e70c242a44891d8fad90caf
mchangye <mchangye@gmail.com>
2023-11-06 17:24:50 +0800

    design indiv sch page page

--- src/views/indivschview.vue
+++ src/views/indivschview.vue
@@ -1,30 +1,46 @@
 <template>
     <body>
         <div v-if="school">
+            <h1>{{ school.School_Name }}</h1>
             <img width=200 height=200 :src=school_image>
             <h2>general info</h2>
-
-            <ul>
+            <table>
+                <tr>
+                    <td class="general_data">Address: {{ school.School_Address }}</td>
+                    <td class="general_data">Postal Code: {{ school.School_Postal_Code }}</td>
+                    <td class="general_data">Region: {{ school.School_Region }}</td>
+                </tr>
+                <tr>
+                    <td class="general_data">School Mode: {{ school.School_Mode }}</td>
+                    <td class="general_data">School Nature: {{ school.School_Nature }}</td>
+                    <td class="general_data">School Type: {{ school.School_Type }}</td>
+                </tr>
+                <tr>
+                    <td class="general_data">School Website: <a :href=school_website_src>{{ school.School_Website }}</a></td>
+                    <td class="general_data">School Email: {{ school.School_Email }}</td>
+                    <td class="general_data">School Number: {{ school.School_Number }}</td>
+                </tr>
+            </table>
+            <!--<ul>
                 <li>{{ school.School_Name }}</li>
                 <li>{{ school.School_Address }}</li>
                 <li>{{ school.School_Code }}</li>
                 <li>{{ school.School_Postal_Code }}</li>
-            </ul>
+            </ul>-->
         </div>
 
 
 
-
-        <div v-if="subjects">
-            <p>
-                <a class="btn btn-primary" @click="toggleCollapseSubjects" role="button" aria-expanded="isCollapsedSubjects"
+        <div class="row">
+        <div class="col-sm-6" v-if="subjects">
+            <p class="dropdown">
+                <a class="btn btn-success" @click="toggleCollapseSubjects" role="button" aria-expanded="isCollapsedSubjects"
                     aria-controls="collapseExampleSubjects">
                     Subjects
                 </a>
             </p>
             <div :class="['collapse', { 'show': isCollapsedSubjects }]" id="collapseExampleSubjects">
-                <h2>subjects</h2>
-                <ul>
+                <ul class="dropdown_data">
                     <li v-for="subject of subjects">
                         {{ subject }}
                     </li>
@@ -33,16 +49,15 @@
         </div>
 
 
-        <div v-if="dsa">
-            <p>
-                <a class="btn btn-primary" @click="toggleCollapseDSA" role="button" aria-expanded="isCollapsedDSA"
+        <div class="col-sm-6" v-if="dsa">
+            <p class="dropdown">
+                <a class="btn btn-success" @click="toggleCollapseDSA" role="button" aria-expanded="isCollapsedDSA"
                     aria-controls="collapseExampleDSA">
                     DSA
                 </a>
             </p>
             <div :class="['collapse', { 'show': isCollapsedDSA }]" id="collapseExampleDSA">
-                <h2>dsa</h2>
-                <ul>
+                <ul class="dropdown_data">
                     <li v-for="dsa of dsa">
                         {{ dsa }}
                     </li>
@@ -51,107 +66,148 @@
         </div>
 
 
-        <div v-if="cca">
-            <p>
-                <a class="btn btn-primary" @click="toggleCollapseCCA" role="button" aria-expanded="isCollapsedCCA"
+        <div class="col-sm-6" v-if="cca">
+            <p class="dropdown">
+                <a class="btn btn-success" @click="toggleCollapseCCA" role="button" aria-expanded="isCollapsedCCA"
                     aria-controls="collapseExampleCCA">
                     CCA
                 </a>
             </p>
             <div :class="['collapse', { 'show': isCollapsedCCA }]" id="collapseExampleCCA">
-                <h2>cca</h2>
-                <ul>
+                <ul class="dropdown_data">
                     <li v-for="cca of cca">
                         {{ cca }}
                     </li>
                 </ul>
             </div>
         </div>
-        <div v-if="affiliations">
-            <p>
-                <a class="btn btn-primary" @click="toggleCollapseAffiliations" role="button"
+        <div class="col-sm-6" v-if="affiliations">
+            <p class="dropdown">
+                <a class="btn btn-success" @click="toggleCollapseAffiliations" role="button"
                     aria-expanded="isCollapsedAffiliations" aria-controls="collapseExampleAffiliations">
                     Affiliated Schools
                 </a>
             </p>
             <div :class="['collapse', { 'show': isCollapsedAffiliations }]" id="collapseExampleAffiliations">
-                <h2>affiliations</h2>
-                <ul>
+                <ul class="dropdown_data">
                     <li v-for="affiliation of affiliations">
                         {{ affiliation }}
                     </li>
                 </ul>
             </div>
         </div>
-        <div v-if="psle">
-            <p>
-                <a class="btn btn-primary" @click="toggleCollapsePSLE" role="button" aria-expanded="isCollapsedPSLE"
+    </div>
+    <div>
+    <div class="row">
+        <div class="col-sm-6" v-if="psle">
+            <p class="dropdown">
+                <a class="btn btn-success" @click="toggleCollapsePSLE" role="button" aria-expanded="isCollapsedPSLE"
                     aria-controls="collapseExamplePSLE">
                     PSLE scores
                 </a>
             </p>
             <div :class="['collapse', { 'show': isCollapsedPSLE }]" id="collapseExamplePSLE">
-                <h2>psle</h2>
-                <ul>
+                <table class="psle_table">
+                    <tr>
+                        <th class="psle_table_data">Standard</th>
+                        <th class="psle_table_data">Affiliation</th>
+                        <th class="psle_table_data">Non-Affiliation</th>
+                    </tr>
+                    <tr>
+                        <td class="psle_table_data">IP</td>
+                        <td class="psle_table_data">{{ psle.IP_Affiliation }}</td>
+                        <td class="psle_table_data">{{ psle.IP_NonAffiliation }}</td>
+                    </tr>
+                    <tr>
+                        <td class="psle_table_data">Express</td>
+                        <td class="psle_table_data">{{ psle.Express_Affiliation }}</td>
+                        <td class="psle_table_data">{{ psle.Express_NonAffiliation }}</td>
+                    </tr>
+                    <tr>
+                        <td class="psle_table_data">Normal Academic</td>
+                        <td class="psle_table_data">{{ psle.NA_Affiliation }}</td>
+                        <td class="psle_table_data">{{ psle.NA_NonAffiliation }}</td>
+                    </tr>
+                    <tr>
+                        <td class="psle_table_data">Normal Technical</td>
+                        <td class="psle_table_data">{{ psle.NT_Affiliation }}</td>
+                        <td class="psle_table_data">{{ psle.NT_NonAffiliation }}</td>
+                    </tr>
+                </table>
+                <!--<ul>
                     <li>IP: {{ psle.IP_NonAffiliation }}</li>
                     <li>Express: {{ psle.Express_NonAffiliation }}</li>
                     <li>Normal(A): {{ psle.NA_NonAffiliation }}</li>
                     <li>Normal(T): {{ psle.NT_NonAffiliation }}</li>
-                </ul>
+                </ul>-->
             </div>
         </div>
-        <div v-if="electives">
-            <p>
-                <a class="btn btn-primary" @click="toggleCollapseElective" role="button" aria-expanded="isCollapsedElective"
+        <div class="col-sm-6" v-if="electives">
+            <p class="dropdown">
+                <a class="btn btn-success" @click="toggleCollapseElective" role="button" aria-expanded="isCollapsedElective"
                     aria-controls="collapseExampleElective">
                     Electives
                 </a>
             </p>
             <div :class="['collapse', { 'show': isCollapsedElective }]" id="collapseExampleElective">
-
-                <h2>electives</h2>
-                <ul v-for="elective of electives">
+                <ul class="dropdown_data" v-for="elective of electives">
                     <li v-for="elective_desc of elective">
                         {{ elective_desc }}
                     </li>
                 </ul>
 
             </div>
-            <div v-if="one_map">
-                <p>
-                    <a class="btn btn-primary" @click="toggleCollapseMap" role="button" aria-expanded="isCollapsedMap"
+        </div>
+        
+            <div  class="col-sm-6" v-if="one_map">
+                <p class="dropdown">
+                    <a class="btn btn-success" @click="toggleCollapseMap" role="button" aria-expanded="isCollapsedMap"
                         aria-controls="collapseExampleMap">
                         Map
                     </a>
                 </p>
                 <div :class="['collapse', { 'show': isCollapsedMap }]" id="collapseExampleMap">
-
+                    <div class="dropdown_data">
                     <h2>Map</h2>
-                    <img :src=one_map alt="map" />
+                    <img :src=one_map alt="map" width="260" height="260"/>
+                    </div>
                 </div>
             </div>
+            <div class="col-sm-6" v-if="special_ed">
+            <p class="dropdown">
+                <a class="btn btn-success" @click="toggleCollapseSpecialEd" role="button" aria-expanded="isCollapsedSpecialEd"
+                    aria-controls="specialedcondition">
+                    Special Education Programmes
+                </a>
+            </p>
+            <div :class="['collapse', { 'show': isCollapsedSpecialEd }]" id="collapseExampleSpecialEd">
+                <ul class="dropdown_data">
+                    <li v-for="specialed of special_ed">
+                        {{ specialed }}
+                    </li>
+                </ul>
+            </div>
+            <!--<div :class="['collapse', {'show': isCollapsedSpecialEd}]" id="collapseSpecialEdError">
+                no special ed programmes
+            </div>-->
         </div>
+        
+    </div>
+    </div>
 
         <div>
             <div>
-            <iframe :src=minimapsrc height="500" width="800" scrolling="no" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+            <iframe :src=minimapsrc height="500" width="800" scrolling="no" frameborder="0" allowfullscreen="allowfullscreen" style="width: 451.333px; height: 451.333px;"></iframe>
             </div>
-            <input type="text" v-model="inputpostalcode" placeholder="please input postal code">
-
-        <button @click="updateminimapsrc">
-            update
-        </button>
         </div>
-        
-
+    
         <div>
-            <h1>Routes</h1>
+            <h1>Find the Quickest Route</h1>
             <input type="text" v-model="originpostal" placeholder="please input origin postal code">
             <button @click="displayroutes">
                 display routes
             </button>
             <div v-if="route_data">
-                <div>Route</div>
                 <div v-for="leg in route_steps">
                     
                     <div v-if="leg.mode == 'BUS'">   
@@ -168,7 +224,46 @@
         </div>
     </body>
 </template>
+<style scoped>
+.dropdown {
+    display: flex;
+    position: relative;
+    margin-left: 90px;
+}
 
+.btn {
+    width: 250px;
+    margin-left: 100px;
+}
+
+.psle_table {
+    border-color: green;
+    border-style: solid;
+    margin-left: 200px;
+}
+
+.psle_table_data {
+    border-color: green;
+    border-style: solid;
+    border-width: 1px;
+    padding: 10px;
+    margin: 10px;
+    
+}
+
+.general_data {
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid black;
+}
+
+.dropdown_data {
+    margin-left: 200px;
+    border: 1px solid black;
+    border-radius: 5%;
+}
+
+</style>
 <script>
 import axios from 'axios';
 
@@ -195,6 +290,7 @@ export default {
             isCollapsedPSLE: false,
             isCollapsedElective: false,
             isCollapsedMap: false,
+            isCollapsedSpecialEd: false,
             minimapsrc: "",
             subset: "",
             inputpostalcode: "",
@@ -204,7 +300,8 @@ export default {
             destlat: "",
             destlong: "",
             route_data: {},
-            route_steps: []
+            route_steps: [],
+            schoolwebsitesrc: ""
 
 
 
@@ -239,6 +336,7 @@ export default {
                     console.log(response.data);
                     this.school = response.data;
                     this.school_image = response.data.School_Image_Source;
+                    this.school_website_src = response.data.School_Website;
                     console.log(this.school);
                     this.new_postal_code = response.data.School_Postal_Code.slice(1,7);
                     console.log(this.new_postal_code);
@@ -288,7 +386,7 @@ export default {
             axios.get('http://localhost:5000/special_ed/' + this.searchID)
                 .then(response => {
                     console.log(response.data);
-                    this.special_ed = response.data.Special_Ed_Programmes;
+                    this.special_ed = response.data.Special_Education_Support;
                     console.log(this.special_ed)
                 })
                 .catch(error => {
@@ -350,6 +448,9 @@ export default {
         toggleCollapseMap() {
             this.isCollapsedMap = !this.isCollapsedMap;
         },
+        toggleCollapseSpecialEd() {
+            this.isCollapsedSpecialEd = !this.isCollapsedSpecialEd;
+        },
         async getmap() {
            await this.GetSchoolDetails();
            try {
@@ -382,17 +483,7 @@ export default {
                     console.error(error);
                 };  
         }, 
-        updateminimapsrc() {
-            if (this.inputpostalcode.length != 6) {
-                alert("Please input a valid postal code");
-                return;
-            }
-            var length = this.minimapsrc.length;
-            this.subset = this.minimapsrc.slice(0, length-14);
-            console.log(this.subset);
-            this.minimapsrc = this.subset + `marker=postalcode:${this.inputpostalcode}!colour:red&popupWidth=200`;
-            console.log(this.minimapsrc);
-        },
+        
         async displayroutes() {
             if (this.originpostal.length != 6) {
                 alert("Please input a valid postal code");
@@ -414,6 +505,12 @@ export default {
                                 this.destlong = response.data.results[0].LONGITUDE;
                                 console.log(this.destlat);
                                 console.log(this.destlong);
+                                //var length = this.minimapsrc.length;
+                                //this.subset = this.minimapsrc.slice(0, length-14);
+                                //console.log(this.subset);
+                                //this.minimapsrc = this.subset + `marker=postalcode:${this.originpostal}!colour:red!rType:TRANSIT!rDest:${this.destlat},${this.destlong}&popupWidth=200`;
+                                //console.log(this.minimapsrc);
+                                this.minimapsrc = `https://www.onemap.gov.sg/amm/amm.html?mapStyle=Night&zoomLevel=15&marker=postalcode:${this.new_postal_code}!colour:red&marker=postalcode:${this.originpostal}!colour:red!rType:TRANSIT!rDest:${this.destlat},${this.destlong}&popupWidth=200`
                                 var current_date = new Date();
                                 console.log(current_date)
                                 var month = ("0" + (current_date.getMonth() + 1)).slice(-2);
